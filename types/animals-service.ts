d41918bc4ca589ad92e9cbbfe9d5ce4d6ef9a168
derekgygax <derekgygax@gmail.com>
2025-01-06 11:41:23 -0500

    types animal to animals-service

--- types/animals-service.ts
+++ types/animals-service.ts
@@ -1,8 +1,16 @@
 import { components } from "@/api-contracts/animals-service/types";
 
+// Animal
 export type HEALTH_TYPE = components["schemas"]["HEALTH_TYPE"];
 export type GENDER = components["schemas"]["GENDER"];
-export type SPECIE = components["schemas"]["SPECIE"];
 export type AnimalBase = components["schemas"]["AnimalBase"];
 export type AnimalIdentifier = components["schemas"]["AnimalIdentifier"];
 export type Animal = components["schemas"]["Animal"];
+
+// Specie
+export type SpecieBase = components["schemas"]["SpecieBase"];
+export type Specie = components["schemas"]["Specie"];
+
+// Medical Record
+
+// Event
