97d9895249c6546362dd61a1e5338971165890a4
derekgygax <derekgygax@gmail.com>
2025-01-09 09:19:56 -0500

    add-animal NOT using AnimalForm. Delete that shitting component. maybe remake it later

--- app/staff/animals/add-animal/page.tsx
+++ app/staff/animals/add-animal/page.tsx
@@ -4,16 +4,24 @@
 import { FORM_CONFIGS } from "@/config/formConfigs";
 
 // types
-import { FORM_NAME, FormConfig } from "@/types/form";
+import { FORM_NAME, FormConfig, SelectorOption } from "@/types/form";
+import { SpecieBase } from "@/types/animals-service";
+
+// server actions
+import { addAnimalAction } from "@/app/_actions/animals-service/animals";
+import { getSpeciesBase } from "@/app/_actions/animals-service/specie";
+
+// utils
+import { toSelectorOptions } from "@/lib/utils/general";
 
 // global components
 import { Title } from "@/app/_components/title/Title";
 
+// client components
+import { ZodForm } from "@/app/_client_components/zodForm/ZodForm";
+
 // layouts
 import { PageSection } from "@/app/_layouts/pageSection/PageSection";
-
-// local components
-import { AnimalForm } from "../../_client_components/animalForm/AnimalForm";
 
 // content
 import { title } from "@/content/app/staff/animals/add-animal";
@@ -25,6 +33,18 @@ export default async function AddAnimalPage() {
 
   const formConfig: FormConfig<FORM_NAME.ADD_ANIMAL> = FORM_CONFIGS[FORM_NAME.ADD_ANIMAL];
 
+
+  // TODO MAKE THE NAME OF THE FIELD THAT NEEDS A SELECTOR DYNAMIC!!
+  // TODO I HATE the hard coding of "specie" here but you gotta move forward
+  // for now and come back and fix later
+  const species: SpecieBase[] = await getSpeciesBase();
+  const selectorOptions: Record<string, SelectorOption[]> = {
+    specie: toSelectorOptions(species, "specie", "specie")
+  };
+
+  // console.log(formConfig.fieldsRequiringFetcheData);
+
+
   return (
     <main>
       <Title
@@ -32,9 +52,11 @@ export default async function AddAnimalPage() {
         level={title.level}
       />
       <PageSection>
-        <AnimalForm
+        <div></div>
+        <ZodForm
           formName={formConfig.name}
-          formLabel={formConfig.label}
+          formServerAction={addAnimalAction}
+          selectorOptions={selectorOptions}
           zodSchemaName={formConfig.zodSchemaName}
         />
       </PageSection>
