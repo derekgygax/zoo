e83bc27a3decf81b3f0ca769f9af01daf30258d9
derekgygax <derekgygax@gmail.com>
2025-02-14 16:06:46 -0500

    hero component set up

--- app/(homepage)/page.tsx
+++ app/(homepage)/page.tsx
@@ -3,12 +3,15 @@
 import { PageSection } from "../_layouts/pageSection/PageSection";
 
 // components
-import { HomePageHero } from "../_components/homePageHero/HomePageHero";
+import { Hero } from "../_components/hero/Hero";
+
+// styles
+import styles from './page.module.scss';
 
 export default function HomePage() {
   return (
     <main>
-      <HomePageHero />
+      <Hero backgroundStyles={styles.heroBackground} />
       <PageSection>
         <span>div</span>
       </PageSection>
